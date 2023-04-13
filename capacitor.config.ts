8ffbf99a38c1b01ed2f437650f7d42d08df64d54
Robin Genz <mail@robingenz.dev>
2023-04-13 15:31:36 +0200

    Add native platforms

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,10 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'dev.robingenz.capacitor.onnxruntimedemo',
   appName: 'capacitor-onnx-runtime-demo',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
 };
 
 export default config;
